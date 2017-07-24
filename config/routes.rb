dafdcb70c2551b5fb58345af8ac74edb3dbddc14
tsubasa <tsubasa3612@gmail.com>
2017-07-24 12:45:03 +0000

    develop

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
   get 'users/show/:name' => 'users#show', as: 'user_show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
   
 end
